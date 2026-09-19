008506e2c0cbb022c24dddf632ef7e1630d558b1
dev000077 <72245977+dev000077@users.noreply.github.com>
2026-09-19 22:19:25 +0530

    OK

--- script.js
+++ script.js
@@ -23,11 +23,11 @@ const points = [
   {club:"AROMA-I (Aroma Boat Club) ", points:10},
   {club:"MELPADOM(Pallathuruthy Boat Club)", points:9},
   {club:"NIRANAM(Niranam Boat Club)", points:8},
-  {club:"VEEYAPURAM", points:7},
-  {club:"ARPOKKARA", points:6},
+  {club:"VEEYAPURAM(UBC FC Kainakary)", points:7},
+  {club:"ARPOOKARA", points:6},
   {club:"CHERUTHANA", points:5},
-  {club:"PUNYALALAN", points:4},
-  {club:"NADUBHAGAM", points:3},
+  {club:"PUNYALALAN NIRANAM", points:4},
+  {club:"NADUBHAGAM(Punnamada Boat Club)", points:3},
   {club:"THALAVADI", points:2}
 ];
 
